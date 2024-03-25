8cc97bb2edae6d540b8575f39882202d03131d76
jayamohan2303 <124357763+jayamohan2303@users.noreply.github.com>
2024-03-25 18:36:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const app = express()
+
 const dbPath = path.join(__dirname, 'goodreads.db')
+
 let db = null
 
 const initializeDBAndServer = async () => {
@@ -21,17 +23,18 @@ const initializeDBAndServer = async () => {
     process.exit(1)
   }
 }
+
 initializeDBAndServer()
 
 app.get('/books/', async (request, response) => {
   const getBooksQuery = `
-  SELECT 
-    * 
-    FROM 
-      book 
-    ORDER BY 
-      book_id;
-  `
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`
   const booksArray = await db.all(getBooksQuery)
   response.send(booksArray)
 })
+
